770e959cd95ab713dd42a5352ca99a0e9ff736a7
Muhammad Zaryaab Shahbaz <zariab64@gmail.com>
2023-02-24 06:46:16 +0500

    Intro to Cloud Security

---
+++ script.js
@@ -0,0 +1,118 @@
+const flagModal = new bootstrap.Modal(document.getElementById("flag"), {});
+
+const options = [
+  { id: 0, name: "Create", hint: "All data is created somewhere." },
+  {
+    id: 1,
+    name: "Store",
+    hint: "It refers to implementing data redundancy and security strategies.",
+  },
+  { id: 2, name: "Use", hint: "How can we utilise the information?" },
+  { id: 3, name: "Share", hint: "Data is constantly being shared." },
+  {
+    id: 4,
+    name: "Archive",
+    hint: "It is all about storing data after its active life.",
+  },
+  {
+    id: 5,
+    name: "Destroy",
+    hint: "It refers to removal of data from storage and archives.",
+  },
+];
+
+const selectStep = (question, answer) => {
+  if (question !== answer) {
+    $(".current").addClass("wrong");
+    return;
+  }
+
+  // mark as correct
+  $(".current").addClass("correct");
+  $(".current").removeClass("wrong current");
+  $(`#dropdown-${question}`).remove();
+  $(`#step-${question}`).removeClass("hidden");
+  $(`#step-${question}`).text(options[question].name);
+
+  if (question === 5) {
+    // last question
+    const flag = atob("VEhNe0NMT1VEXzExMTAxfQ==");
+    $("#flag-text").text(flag);
+    flagModal.toggle();
+    return;
+  }
+
+  // change step
+  $(".disabled")
+    .slice(0, 4)
+    .addClass("current");
+  $(".disabled")
+    .slice(0, 4)
+    .removeClass("disabled");
+};
+
+const createToggle = (id, cls) => {
+  const itemClass =
+    "dropdown-item text-white cursor-pointer hover:rounded hover:transition hover:ease-in hover:duration-300";
+  let content = `<div class="dropdown mb-3" id="dropdown-${id}">
+    <button class="btn neutral text-white w-100 dropdown-toggle ${cls}"
+      type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      Select Step
+    </button>
+    <ul class="dropdown-menu w-100 neutral text-white py-0">`;
+  const items = [...options].sort(() => Math.random() - 0.5);
+  content += items
+    .map(option => {
+      return `<li><p class="${itemClass}" onClick="selectStep(${id},${option.id})">${option.name}</p></li>`;
+    })
+    .join("");
+  content += `</ul></div>`;
+
+  return content;
+};
+
+const createTemplate = (index, ping = true) => {
+  const item = options[index];
+  const status = index === 0 ? "current" : "disabled";
+  const className = `${ping ? "" : "flex-row-reverse"}`;
+  return `
+    <div class="relative flex justify-between ${className} items-center w-full">
+      <div class="order-1 w-5/12"></div>
+      <div class="border-2-2 left-1/2 absolute border-opacity-20 h-full border ${status}"></div>
+      <div class="circle z-20 flex items-center order-1 bg-gray-500 shadow-xl w-8 h-8 rounded-full ${status} 
+      flex items-center justify-center">
+        <h1 class="mx-auto font-semibold text-lg text-white">${index + 1}</h1>
+      </div>
+      <div class="template order-1 rounded-lg shadow-xl w-5/12 px-6 py-4 ${status}">
+        ${createToggle(index, status)}
+        <h3 
+          class="mb-3 font-bold text-gray-800 text-xl hidden" 
+          id="step-${index}">${item.name}</h3>
+        <p class="leading-snug tracking-wide text-gray-900 text-opacity-100">
+        ${item.hint}
+        </p>
+      </div>
+    </div>`;
+};
+
+$(function() {
+  const tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  [...tooltips].map(t => new bootstrap.Tooltip(t));
+
+  const content = $("#content");
+  const templates = options
+    .map((option, index) => createTemplate(index, (index + 1) % 2 !== 0))
+    .join("");
+  content.append(templates);
+});
+
+const copyText = (label = "flag-text", container = "flag-container") => {
+  const text = $(`#${label}`).text();
+  navigator.clipboard.writeText(text);
+
+  const tooltip = bootstrap.Tooltip.getInstance(`#${container}`);
+  tooltip.setContent({ ".tooltip-inner": "Copied!" });
+  setTimeout(() => {
+    tooltip.setContent({ ".tooltip-inner": "Copy to clipboard" });
+  }, 2000);
+};
